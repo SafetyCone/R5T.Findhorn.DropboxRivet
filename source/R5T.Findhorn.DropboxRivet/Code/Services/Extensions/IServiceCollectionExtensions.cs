849c6636dc94fdbbb50923f3551a16c7682664ee
MinexAutomation <minex.automation@gmail.com>
2020-01-30 16:29:47 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Findhorn.DropboxRivet/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Findhorn.DropboxRivet/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
 using R5T.Dalkeith.Standard;
 using R5T.Dimitrios.DropboxRivet;
 using R5T.Findhorn.Dimitrios;
@@ -12,16 +13,27 @@ namespace R5T.Findhorn.DropboxRivet
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="IDataDirectoryPathProvider"/> service.
+        /// </summary>
         public static IServiceCollection AddDataDirectoryPathProvider(this IServiceCollection services)
         {
-            services
-                .AddSingleton<IDataDirectoryPathProvider, OrganizationDataDirectoryPathProvider>()
-                .AddDataDirectoryNameConvention()
-                .AddDropboxRivetOrganizationDirectoryPathProvider()
-                .AddStringlyTypedPathOperator()
+            services.AddOrganizationDataDirectoryPathProvider(
+                services.AddDropboxRivetOrganizationDirectoryPathProviderAction(),
+                services.AddDataDirectoryNameConventionAction(),
+                services.AddStringlyTypedPathOperatorAction())
                 ;
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IDataDirectoryPathProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IDataDirectoryPathProvider> AddDataDirectoryPathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IDataDirectoryPathProvider>(() => services.AddDataDirectoryPathProvider());
+            return serviceAction;
         }
     }
 }
